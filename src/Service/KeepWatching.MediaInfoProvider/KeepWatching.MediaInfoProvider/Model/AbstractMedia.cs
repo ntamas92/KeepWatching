84478ba69ea931d45e63ae14130a30ffc4025c93
Nagy Tamás <nredline@gmail.com>
2019-06-17 09:30:02 +0200

    Init datamodel and tmdb api connection
    Init datamodel and tmdb api connection

---
+++ src/Service/KeepWatching.MediaInfoProvider/KeepWatching.MediaInfoProvider/Model/AbstractMedia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KeepWatching.MediaInfoProvider.Model
+{
+    public abstract class AbstractMedia
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime Released { get; set; }
+
+        public uint Length { get; set; }
+
+        public string Genre { get; set; } //TODO: How to represent multiple genres?
+
+        public IEnumerable<Person> Directors { get; set; }
+
+        public IEnumerable<Person> Writers { get; set; }
+
+        public IEnumerable<Person> Actors { get; set; }
+
+        public string Plot { get; set; }
+
+        public IEnumerable<Rating> Ratings { get; set; }
+
+        public string PosterPath { get; set; }
+    }
+}
