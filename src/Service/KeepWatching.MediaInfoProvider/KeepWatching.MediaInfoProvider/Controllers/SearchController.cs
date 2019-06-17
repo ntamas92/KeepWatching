5d24bd19a778ddcf97c2602a60ed4b247518ddba
Nagy Tamás <nredline@gmail.com>
2019-06-17 09:30:02 +0200

    Init datamodel and tmdb api connection

---
+++ src/Service/KeepWatching.MediaInfoProvider/KeepWatching.MediaInfoProvider/Controllers/SearchController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using KeepWatching.MediaInfoProvider.Model;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace KeepWatching.MediaInfoProvider.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MediaController : ControllerBase
+    {
+        
+
+        //https://api.themoviedb.org/3/search/multi?query=avengers&api_key=6ef8fba42d9f1bbcfe1130a0d9a9ae37
+
+        //Search only TV Shows and Movies, episodes shall be redirected from the TV show homepage
+        [HttpGet]
+        public async Task<IEnumerable<AbstractMedia>> Get(string title)
+        {
+
+            QueryBuilder queryBuilder = new QueryBuilder();
+            queryBuilder.Add("query", title);
+            queryBuilder.Add("api_key", "6ef8fba42d9f1bbcfe1130a0d9a9ae37");
+
+            UriBuilder uriBuilder = new UriBuilder("https", "api.themoviedb.org");
+            uriBuilder.Path = "3/search/multi";
+            uriBuilder.Query = queryBuilder.ToQueryString().ToUriComponent();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(uriBuilder.Uri);
+                string result = await response.Content.ReadAsStringAsync();
+
+                IEnumerable<AbstractMedia> movies = JObject.Parse(result)
+                    .GetValue("results")
+                    .Children()
+                    .Where(x => x.Value<string>("media_type").Equals("movie"))
+                    .Select(x => x.Value<string>("title"))
+                    .Select(x => new Movie() { Title = x });
+
+                return movies;
+            }
+        }
+
+        //[HttpGet]
+        //public ActionResult<AbstractMedia> Get(string id)
+        //{
+
+        //}
+
+    }
+}
---
+++ src/Service/KeepWatching.MediaInfoProvider/KeepWatching.MediaInfoProvider/Model/Movie.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KeepWatching.MediaInfoProvider.Model
+{
+    internal class Movie : AbstractMedia
+    {
+    }
+}
