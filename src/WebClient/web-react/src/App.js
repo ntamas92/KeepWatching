c7908c3a18ae1a531d216a6f69ad05b172690303
Nagy Tamás <nredline@gmail.com>
2019-06-30 23:25:00 +0200

    Filter unnecessary queries from search

--- src/WebClient/web-react/src/App.js
+++ src/WebClient/web-react/src/App.js
@@ -2,48 +2,63 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component{
+  render() {
+    return(
+      <div className="App">
+        <Search/>
+      </div>
+    )
+  }
+}
 
+// Probably we don't need to create so many components..
+class Search extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      executingQuery: null,
+      pendingQuery: null,
       data: null,
-      query: '',
     };
   }
 
-  componentDidMount() {
-    // this.handleInputChange()
+  getSearchResult = (query) => {
+    var fetchResult = this.fetchSearchResult(query);
+    
+    fetchResult.then(x => {
+        var pq = this.state.pendingQuery;
+        this.setState({executingQuery:pq, pendingQuery:null, data: x}, () => {
+          if(this.state.executingQuery !== null){
+            this.getSearchResult(this.state.executingQuery)
+        }})
+      })
   }
 
+  fetchSearchResult = (query) => {
+      if(query === ''){
+        return new Promise((resolve, reject) => resolve([])) 
+      }
+      else{
+        return fetch('https://localhost:44367/api/media?title=' + query)
+          .then(response => response.json())
+          .then(x => { return x.map(fun => ({'title':fun.title, 'image':fun.posterPath}))})
+      }
+  }
 
-  getNames = () => {
-    if(this.state.query === ''){
-      this.setState({data:[]})
+  handleInputChange = (event) => {
+    if(this.state.executingQuery === null){
+      this.setState({executingQuery:event.target.value}, () => this.getSearchResult(this.state.executingQuery))
     }
     else{
-      fetch('https://localhost:44367/api/media?title=' + this.state.query)
-      .then(response => response.json())
-      .then(x => this.setState({data: x.map(fun => ({'title':fun.title, 'image':fun.posterPath}))}));
+      this.setState({pendingQuery:event.target.value})
     }
-  }
-
-  handleInputChange = () => {
-    this.setState({query: this.search.value}, () => {
-      this.getNames()
-    });
-  }
-
-  handleChange = (event) => {
-    this.setState({query: event.target.value}, () => {
-      this.getNames()
-    });
   }
 
   render() {
     return(
-      <div className="App">
-        <SearchBar onInputChanged={this.handleChange}/>
+      <div className="search" style={{width:'250px'}}>
+        <SearchBar onInputChanged={this.handleInputChange}/>
         <SearchResults results={this.state.data} />
       </div>
     )
@@ -54,14 +69,15 @@ class SearchResults extends Component{
   render(){
     if(this.props.results){
       return this.props.results.filter(element => element.image !== null && element.title !== null).map(element => 
-        <div className="resultRow">
-          <img src={element.image} alt={element.title}/>
+        <div className="result-row" style={{overflow:'auto'}}>
+          
+          <img src={element.image} alt={element.title} style={{float:'left'}} height={100}/>
           <p>{element.title}</p>
         </div>          
       )
     }
     else{
-      return <div className="emptyResult"/>
+      return <div className="empty-result"/>
     }
     
   }
@@ -82,5 +98,6 @@ class SearchBar extends Component{
     )
   }
 }
+
 
 export default App;
