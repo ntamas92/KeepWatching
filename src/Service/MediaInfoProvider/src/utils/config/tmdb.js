4b3226e6a08128a1e48c94943c85a8530db7492d
Tamas Nagy <nredline@gmail.com>
2020-11-03 22:52:11 +0100

    Move tmdb constants to config/tmdb

--- src/Service/MediaInfoProvider/src/utils/config/tmdb.js
+++ src/Service/MediaInfoProvider/src/utils/config/tmdb.js
@@ -1,10 +1,19 @@
-const TMDB_API_PREFIX = "https://api.themoviedb.org/3"
+const API_PREFIX = "https://api.themoviedb.org/3"
 
-const MULTI_SEARCH_ENDPOINT = "/search/multi"
+const MULTI_SEARCH_ENDPOINT = "search/multi"
 
-const TMDB_API_KEY = process.env.TMDB_API_KEY
+const MOVIE_DETAILS_ENDPOINT = "movie"
+
+const TV_DETAILS_ENDPOINT = "tv"
+
+const PERSON_DETAILS_ENDPOINT = "person"
+
+const API_KEY = process.env.TMDB_API_KEY
 
 const THUMBNAIL_PATH_PREFIX = "https://image.tmdb.org/t/p/w92"
 
+const BACKDROP_PATH_PREFIX = "https://image.tmdb.org/t/p/original"
 
-module.exports = { TMDB_API_KEY, TMDB_API_PREFIX, MULTI_SEARCH_ENDPOINT, THUMBNAIL_PATH_PREFIX }
+
+module.exports = { API_KEY, API_PREFIX, MULTI_SEARCH_ENDPOINT, MOVIE_DETAILS_ENDPOINT, TV_DETAILS_ENDPOINT, PERSON_DETAILS_ENDPOINT, 
+  THUMBNAIL_PATH_PREFIX, BACKDROP_PATH_PREFIX }
